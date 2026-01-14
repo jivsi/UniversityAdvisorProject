3d51781f6b7a848c1b8bf3289b05eeaed0d1a222
jivsi <jivkoyordanov07@gmail.com>
2026-01-15 01:13:48 +0200

    ngrok

--- obj/Debug/net8.0/UniversityFinder.AssemblyInfo.cs
+++ obj/Debug/net8.0/UniversityFinder.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversityFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e28200a7127df472e2fcdd8c0fa340d2eb6fa68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20edcb4f7b464503115239170d0799b5cd37a023")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversityFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversityFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
