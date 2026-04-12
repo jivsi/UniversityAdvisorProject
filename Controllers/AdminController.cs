096ac9603151f43568d826f54e6c70512912b7e2
jivsi <jivkoyordanov07@gmail.com>
2026-04-12 11:44:05 +0300

    reverted some bad updates

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -600,34 +600,18 @@ namespace UniversityFinder.Controllers
         // ===================== PROGRAM MANAGEMENT =====================
 
         [HttpGet]
-        public async Task<IActionResult> ManagePrograms(Guid id, bool fullCatalog = false)
+        public async Task<IActionResult> ManagePrograms(Guid id)
         {
             var university = await _supabaseService.GetUniversityByIdAsync(id);
             if (university == null) return NotFound();
 
             var programs = await _supabaseService.GetProgramsAsync(id);
             var categories = await _supabaseService.GetSubjectCategoriesAsync();
-            List<Subject> subjects;
-            var pickerFallbackToFull = false;
-            if (fullCatalog)
-            {
-                subjects = await _supabaseService.GetSubjectsAsync();
-            }
-            else
-            {
-                subjects = await _supabaseService.GetSubjectsOfferedViaProgramsAsync();
-                if (subjects.Count == 0)
-                {
-                    subjects = await _supabaseService.GetSubjectsAsync();
-                    pickerFallbackToFull = true;
-                }
-            }
+            var subjects = await _supabaseService.GetSubjectsAsync();
 
             ViewBag.University = university;
             ViewBag.Categories = categories;
             ViewBag.Subjects = subjects;
-            ViewBag.FullCatalog = fullCatalog;
-            ViewBag.PickerFallbackToFullCatalog = pickerFallbackToFull;
 
             return View(programs);
         }
