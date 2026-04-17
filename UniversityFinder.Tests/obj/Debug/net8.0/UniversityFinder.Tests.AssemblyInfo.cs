7f3af9ffdee3496dd3b9a04f4e0ad44da843bbf2
jivsi <jivkoyordanov07@gmail.com>
2026-04-17 11:07:40 +0300

    lil change

--- UniversityFinder.Tests/obj/Debug/net8.0/UniversityFinder.Tests.AssemblyInfo.cs
+++ UniversityFinder.Tests/obj/Debug/net8.0/UniversityFinder.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversityFinder.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96e6a1fe5d992fd30dc97211d4378e1fc5a5111")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88efdf95cea90e8900d82a27223992420d937253")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversityFinder.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversityFinder.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
